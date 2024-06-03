35ed39ecf51926fa26226f2077bf4969c51ae954
Karthika Raghavan <karthika.raghavan@gmail.com>
2024-06-02 21:41:50 -0700

    fixing integration tests

--- testing1/tests/integration_tests.py
+++ testing1/tests/integration_tests.py
@@ -1,17 +1,35 @@
 import unittest
 import pika
 import json
+import time
+
+RABBITMQ_HOST = "rabbitmq"
+RABBITMQ_PORT = 5672
+RABBITMQ_QUEUE = 'test'
+RABBITMQ_USERNAME = 'guest'
+RABBITMQ_PASSWORD = 'guest'
+BLOCKED_CONNECTION_TIMEOUT = 300
+
+data = {
+    "name": "John Doe",
+    "email": "john.doe@example.com",
+    "id": 1234,
+    "timestamp": time.time()
+}
+
+# Convert the Python dictionary to a JSON string
+DATA_PAYLOAD = json.dumps(data)
 
 class TestRabbitMQ(unittest.TestCase):
     def setUp(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
         self.channel = self.connection.channel()
 
     def tearDown(self):
         self.connection.close()
 
     def test_data_pushed_to_rabbitmq(self):
-        queue = 'my_queue'
+        queue = RABBITMQ_QUEUE
         self.channel.queue_declare(queue=queue)
 
         def callback(ch, method, properties, body):
@@ -20,8 +38,9 @@ class TestRabbitMQ(unittest.TestCase):
         self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
         self.channel.start_consuming()
 
-        expected_data = {'key': 'value'}  # Replace with your expected data
-        self.assertEqual(self.data, expected_data)
+        self.assertEqual(self.data["name"], "John Doe")
+        self.assertEqual(self.data["email"], "john.doe@example.com")
+        self.assertEqual(self.data["id"], 1234)
 
 if __name__ == '__main__':
     unittest.main()
